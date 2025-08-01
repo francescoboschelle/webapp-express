72198fddc0fee16d66f02af93b72d42fb0c809e0
francescoboschelle <francibossch@gmail.com>
2025-08-01 16:03:34 +0200

    Add CORS support to server and update package dependencies

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 import e from "express";
 import moviesRouter from "./routers/movies.js";
 import { errorHandler, routeNotFound } from "./middlewares/middleware.js";
+import cors from "cors";
 
 const app = e();
 const PORT = 3030;
 
 app.use(e.json());
 app.use(e.static("images"));
+app.use(cors());
+
 app.use("/api/movies", moviesRouter);
 
 app.get("/", (req, res) => {
